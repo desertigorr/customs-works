e6cd6eec674c94e4efb1c2e2d9c2269a2651eb9b
IIgor17 <79518940015a@gmail.com>
2022-08-31 20:55:41 +0300

    fixed link

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-gsap.from('.navigation-socials', {y: -50, opacity: 0, duration: 1, delay: .5})
-gsap.from('.navigation-socials__link', {opacity: 0, stagger: .3, delay: .8})
+gsap.from('.navigation-socials', {y: -50, opacity: 0, duration: 1, delay: .7})
+gsap.from('.navigation-socials__link', {opacity: 0, stagger: .3, delay: .9})
 
 gsap.from('.navigation-content', {y: -50, opacity: 0, duration: 1, stagger: .3})
-// gsap.from('.navigation-content__link', {opacity: 0, stagger: -.3, delay: .8})
+gsap.from('.navigation-content__link', {opacity: 0, stagger: -.3, delay: 1})
 
-gsap.from('.cover-info__img', {x: 200, opacity: 0, duration: .8, delay: .5})
-gsap.from('.cover-info__content', {x: -200, opacity: 0, duration: .8, delay: .8})
+gsap.from('.cover-info__img', {x: 200, opacity: 0, duration: .8, delay: .7})
+gsap.from('.cover-info__content', {x: -200, opacity: 0, duration: .8, delay: 1})
